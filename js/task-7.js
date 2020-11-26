4a72a9b13ccce8bd5b662a1c6e72d375e0203aec
Ruslana Kravchenko <ruslana.kravchenko24@gmail.com>
2020-11-26 14:05:29 +0200

    Вносит правки в 7 задание

--- js/task-7.js
+++ js/task-7.js
@@ -2,7 +2,6 @@ const inputRef = document.querySelector('#font-size-control');
 const spanRef = document.querySelector('#text');
 
 const changeSize = () => {
-  console.log(inputRef.value);
   spanRef.style.fontSize = `${inputRef.value}px`;
 };
 inputRef.addEventListener('input', changeSize);
